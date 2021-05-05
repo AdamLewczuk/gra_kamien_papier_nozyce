664ca67018d48ed8a97b5719fa01fb98ed0260e6
Adam Lewczuk <biuroaxweb@gmail.com>
2021-05-05 22:56:09 +0200

    nowe testy

--- funkcje.py
+++ funkcje.py
@@ -1,3 +1,4 @@
+#funkcja wypisująca ruch. Zwraca ruch wybrany przez gracza po kliknięciu odpowiedniego klawisza
 import random
 def wypisz_ruch(wybierz):
     if wybierz == '1':
@@ -7,8 +8,31 @@ def wypisz_ruch(wybierz):
     if wybierz == '3':
         return "Nożyce"
 
+
+#funkcja losująca. Losuje jako komputer między konkretnym ruchem.
+
 def komputer_losuje():
     losowanie = ["Kamień", "Papier", "Nożyce"]
     wynik = (random.choice(losowanie))
     return wynik
 
+# funkcja sprawdzająca płeć po wpisaniu imienia. Zwraca wartość True gdy kobieta i False gdy mężczyzna. 
+
+def czy_kobieta(imie):
+    if imie == "Kuba":
+        return False
+    elif imie[-1] == "a":
+        return True
+    else:
+        return False
+
+# funkcja sprawdzająca końcowy wynik. Zwraca wartość w postaci wyniku. 
+
+def sprawdzwynik(wybierz,wynik):
+    if wybierz == "Kamień" and wynik == "Kamień":
+        return "Jest remis"
+    if wybierz == "Kamień" and wynik == "Papier":
+        return "Przegrywasz"
+    if wybierz == "Kamień" and wynik == "Nożyce":
+        return "Wygrywasz"
+    
