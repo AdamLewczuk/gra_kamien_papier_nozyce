80b68c0bdf44d6f04c1e42c69aa544c1011c543a
Adam Lewczuk <biuroaxweb@gmail.com>
2021-05-06 00:12:16 +0200

    Nie dziala

--- funkcje.py
+++ funkcje.py
@@ -1,3 +1,4 @@
+#funkcja wypisująca ruch. Zwraca ruch wybrany przez gracza po kliknięciu odpowiedniego klawisza
 import random
 def wypisz_ruch(wybierz):
     if wybierz == '1':
@@ -7,8 +8,34 @@ def wypisz_ruch(wybierz):
     if wybierz == '3':
         return "Nożyce"
 
+
+#funkcja losująca. Losuje jako komputer między konkretnym ruchem.
+
 def komputer_losuje():
     losowanie = ["Kamień", "Papier", "Nożyce"]
     wynik = (random.choice(losowanie))
     return wynik
 
+# funkcja sprawdzająca płeć po wpisaniu imienia. Zwraca wartość True gdy kobieta i False gdy mężczyzna. 
+
+def czy_kobieta(imie):
+    if imie == "Kuba":
+        return False
+    elif imie[-1] == "a":
+        return True
+    else:
+        return False
+
+# funkcja sprawdzająca końcowy wynik. Zwraca wartość w postaci wyniku. 
+
+def sprawdzwynik(wybierz,wynik):
+    if wybierz == "1" and wynik == "Kamień":
+        x = "Jest remis"
+        return x
+    elif wybierz == "1" and wynik == "Papier":
+        x = "Przegrywasz"
+        return x
+    elif wybierz == "1" and wynik == "Nożyce":
+        x = "Wygrywasz"
+        return x
+   
